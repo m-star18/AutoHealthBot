b2f315dacfc1cd64c971abd9619462f92f18fd2d
m-star18 <ryuuchei@ezweb.ne.jp>
2020-11-22 23:01:24 +0900

    Fix test.py

--- test/test.py
+++ test/test.py
@@ -1,16 +1,9 @@
-import unittest
-
 from src.job import AutoHealthJob
 
 
-class MyTestCase(unittest.TestCase):
-    def __init__(self):
-        super().__init__()
-        self.job = AutoHealthJob(option=True)
-
-    def test(self):
-        self.assertEqual(self.job.state, True)
+def main():
+    AutoHealthJob()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()

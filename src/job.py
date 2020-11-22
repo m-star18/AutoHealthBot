64970cf3f673d967f5d324cbeeb425d536202781
m-star18 <ryuuchei@ezweb.ne.jp>
2020-11-22 18:16:10 +0900

    Update job.py

---
+++ src/job.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import chromedriver_binary
+
+from src.const import (
+    MICROSOFT_EMAIL,
+    MICROSOFT_PASSWORD,
+    MICROSOFT_LOGIN_URL,
+    MICROSOFT_FORM_URL,
+    MIN_TEMPERATURE,
+    MAX_TEMPERATURE,
+    STATE_CONST,
+)

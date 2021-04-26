63a7259a0817ccc749d7a3e81bf8ab89bc7c6aee
m-star18 <31807@toyota.kosen-ac.jp>
2021-04-26 16:03:26 +0900

    Update const.py

--- src/const.py
+++ src/const.py
@@ -1,6 +1,9 @@
 MICROSOFT_LOGIN_URL = "https://login.microsoftonline.com/"
 MICROSOFT_FORM_URL = "https://forms.office.com/Pages/ResponsePage.aspx?id=XYP-cpVeEkWK4KezivJfyMh6mdFa-OFBkHsyBFa3fSVUODFSWUZGUU1TSzFGWVFFOE1MSjI1NkhOViQlQCN0PWcu "
 
+MICROSOFT_EMAIL = ""
+MICROSOFT_PASSWORD = ""
+
 MIN_TEMPERATURE = 35.6
 MAX_TEMPERATURE = 36.5
 
